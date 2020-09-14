00cbb18fe9c095d3aef9df34ef03cb674c94f457
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-09-14 20:59:40 +0000

    Automatically backed up by Learn

--- lib/while_do_test.rb
+++ lib/while_do_test.rb
@@ -1,6 +1,13 @@
 #while -1 do
 #  puts "sy this forever"
 #end
-while nil do
-  puts "I will never run"
+#while nil do
+#  puts "I will never run"
+#end
+
+count = 0
+while count >5 && <10 do
+  puts "I am #{count}, I love to count"
+  count= count +1
 end
+  

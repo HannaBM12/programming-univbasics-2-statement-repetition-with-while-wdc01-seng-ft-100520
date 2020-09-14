e454838b1f76cc0c68f231464216f42d8402ebc0
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-09-14 21:41:34 +0000

    Automatically backed up by Learn

--- lib/while_do_test.rb
+++ lib/while_do_test.rb
@@ -11,9 +11,17 @@
 #  count+=2
 #end
 
+#magic_exit_number = 7
+#count = 0
+#while count <10 && count !=magic_exit_number do
+#  puts "I am #{count}, I love to count!"
+#  count +=1
+#end
+
 magic_exit_number = 7
 count = 0
-while count <10 && count !=magic_exit_number do
-  puts "I am #{count}, I love to count!"
+while count <10 do
+  break if count == magic_exit_number
+  puts = "I am #{count}, I love to count!"
   count +=1
 end
